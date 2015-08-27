c909bdd4b71eb9365d6e31fc065e4fd8ac1310c2
panchox340 <fcorodriguez4@gmail.com>
2015-08-27 19:52:57 -0400

    primer cambio
    Cree el  formulario para registrar a una persona

---
+++ TuLibroWeb/Registro.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Registro.aspx.cs" Inherits="TuLibroWeb.Registro" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <center>
+            <asp:TableRow>
+                <asp:TableCell>
+                    <asp:Label ID="LblNombreUsuario" Text="Ingrese Su Nombre" runat="server" />
+                </asp:TableCell>                    
+                <asp:TableCell>
+                    <asp:TextBox ID="TxtNombreUsuario" runat="server" />
+                </asp:TableCell>
+                <asp:TableCell>
+                    <asp:RequiredFieldValidator runat="server" ForeColor="Red"
+                    ErrorMessage=" * Ingrese un Nombre en el campo"
+                    ControlToValidate="TxtNombreUsuario" ID="RfvNombreUsuario" />
+                </asp:TableCell>                
+            </asp:TableRow>
+        </center>    
+    </div>
+    </form>
+</body>
+</html>
---
+++ TuLibroWeb/Registro.aspx.designer.cs
@@ -0,0 +1,51 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace TuLibroWeb {
+    
+    
+    public partial class Registro {
+        
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Control LblNombreUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblNombreUsuario;
+        
+        /// <summary>
+        /// Control TxtNombreUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtNombreUsuario;
+        
+        /// <summary>
+        /// Control RfvNombreUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RfvNombreUsuario;
+    }
+}
