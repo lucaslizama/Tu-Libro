0132d083c643de8ede32ea5e24682f595edea344
lucaslizama <lucaslizama3@hotmail.com>
2015-09-14 23:49:42 -0300

    Avances Vista para los Libros
    -Empece a editar la vista de los libros.
    -La vista solo contiene una imagen, un bloque para la informacion del
    libro, un bloque para la descripcion del libro y un boton para agregar
    el libro al carro.

---
+++ TuLibroWeb/VistaLibro.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/PaginaMaestra.Master" AutoEventWireup="true" CodeBehind="VistaLibro.aspx.cs" Inherits="TuLibroWeb.VistaLibro" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="contenidoBarraIzquierda" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="contenidoCentro" runat="server">
+    <fieldset id="fichaProducto">
+        <legend>Ficha del Producto</legend>
+        <div id="contenidoVistaLibro">
+            <img id="thumbnailVistaLibro" class="thumbnailVistaLibro" src="~/recursos/imagenes/book-thumbnails/hg-02.jpg" alt="a" runat="server" />
+            <p id="infoVistaLibro" class="infoVistaLibro" runat="server">asdd</p>
+            <asp:ImageButton ID="ImageButton1" class="btnVistaLibro" runat="server" ImageUrl="recursos/imagenes/agregar.png" />
+            <p id="descVistaLibro" class="descVistaLibro" runat="server">asd</p>
+        </div>
+    </fieldset>
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="contenidoBarraDerecha" runat="server">
+</asp:Content>
---
+++ TuLibroWeb/VistaLibro.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using BibliotecaTuLibro;
+
+namespace TuLibroWeb
+{
+    public partial class VistaLibro : System.Web.UI.Page
+    {
+        private List<Libro> _libros;
+        private List<Usuario> _usuarios;
+        private Stock _stock;
+        private CarritoCompra _carrito;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            InicializarUsuarios();
+            InicializarStock();
+            InicializarCarrito();
+
+
+        }
+
+        private void InicializarUsuarios()
+        {
+            if (Session["usuarios"] == null)
+            {
+                _usuarios = new List<Usuario>();
+                Session["usuarios"] = _usuarios;
+            }
+            else
+            {
+                _usuarios = Session["usuarios"] as List<Usuario>;
+            }
+        }
+
+        private void InicializarStock()
+        {
+            if (Session["stock"] == null)
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(Server.MapPath("~/libros.xml"));
+                _stock = new Stock();
+                _stock.CargarStock(doc);
+                _libros = Stock.ListarLibros(_stock.Libros);
+                Session["stock"] = _stock;
+                Session["libros"] = _libros;
+            }
+            else
+            {
+                _stock = Session["stock"] as Stock;
+                _libros = Session["libros"] as List<Libro>;
+            }
+        }
+
+        private void InicializarCarrito()
+        {
+            if (Session["carrito"] == null)
+            {
+                _carrito = new CarritoCompra();
+                Session["carrito"] = _carrito;
+            }
+            else
+            {
+                _carrito = Session["carrito"] as CarritoCompra;
+            }
+        }
+    }
+}
